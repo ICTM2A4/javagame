dfb306bb64a076d144133f137dd3f2008e61cf94
Jochem Boon <s1138498@student.windesheim.nl>
2020-04-17 10:17:15 +0200

    Ongebruikte referenties weghalen

--- src/main/java/nl/ictm2a4/javagame/Main.java
+++ src/main/java/nl/ictm2a4/javagame/Main.java
@@ -3,8 +3,6 @@ package nl.ictm2a4.javagame;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
