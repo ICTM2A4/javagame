50f7817a5274222c66e52682bfe5c7d7c9250ef1
Sietse Bijlsma <sietsebijlsma@gmail.com>
2020-04-17 09:54:37 +0200

    Main Menu toegevoegd

---
+++ src/main/java/nl/ictm2a4/javagame/MainMenu.java
@@ -0,0 +1,49 @@
+package nl.ictm2a4.javagame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends JFrame implements ActionListener {
+
+    private JButton start, selectlevel, levelbuilder, exit;
+    private static GameScreen screen;
+
+    public MainMenu() {
+        setLayout(new FlowLayout());
+        setSize(320, 360);
+        setTitle("Main Menu");
+
+        start = new JButton("Start");
+        start.addActionListener(this);
+        add(start);
+        selectlevel = new JButton("Select Bevel");
+        selectlevel.addActionListener(this);
+        add(selectlevel);
+        levelbuilder = new JButton("Level Builder");
+        levelbuilder.addActionListener(this);
+        add(levelbuilder);
+        exit = new JButton("Exit");
+        exit.addActionListener(this);
+        add(exit);
+
+        setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == start) {
+            System.out.println("Level Start");
+        }
+        if(e.getSource() == selectlevel) {
+            System.out.println("Level Select start op");
+        }
+        if(e.getSource() == levelbuilder) {
+            System.out.println("Level builder start op");
+        }
+        if(e.getSource() == exit) {
+            System.exit(0);
+        }
+    }
+}
