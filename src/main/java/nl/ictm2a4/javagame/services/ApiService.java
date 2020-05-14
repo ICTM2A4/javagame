478d26651d5c47fd3e6a4b1f7e8c276ece5d0b71
Jochem Boon <s1138498@student.windesheim.nl>
2020-05-14 13:14:11 +0200

    Ondersteuning voor PUT en POST toegevoegd aan de ApiService

--- src/main/java/nl/ictm2a4/javagame/services/ApiService.java
+++ src/main/java/nl/ictm2a4/javagame/services/ApiService.java
@@ -5,10 +5,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -29,10 +26,31 @@ public class ApiService {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(method);
 
+            if(method == "POST" || method == "PUT"){
+
+                int length = body.getBytes(("UTF-8")).length;
+
+                connection.setRequestProperty("Content-Length", String.valueOf(length));
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                    connection.setDoOutput(true);
+
+                    try(OutputStream os = connection.getOutputStream()) {
+                        byte[] input = body.getBytes("utf-8");
+                        os.write(input, 0, input.length);
+                }
+            }
+
             // Request sturen en verwerken
             responseCode = connection.getResponseCode();
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(connection.getInputStream()));
+
+            if(responseCode == 204){
+                // Niet ombouwen naar JSON, 204 is no content
+                return new Response(responseCode, new JSONObject());
+            }
+            // Ombouwen naar Json
 
             JSONParser parser = new JSONParser();
             Reader rd = new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8);
@@ -48,14 +66,7 @@ public class ApiService {
             in.close();
             connection.disconnect();
 
-            // Ombouwen naar Json
             return new Response(responseCode, responseBody);
-        } catch (ProtocolException e) {
-            System.out.println("Request failed");
-            return null;
-        } catch (MalformedURLException e) {
-            System.out.println("Request failed");
-            return null;
         } catch (IOException e) {
             System.out.println("Request failed");
             return null;
