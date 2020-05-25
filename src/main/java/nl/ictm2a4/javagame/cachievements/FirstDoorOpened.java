03a7eb596a63fdb6910ff3fedd50906da1f3ff22
Jochem Boon <s1138498@student.windesheim.nl>
2020-05-25 14:27:13 +0200

    Achievement aan ingelogde gebruiker toevoegen

--- src/main/java/nl/ictm2a4/javagame/cachievements/FirstDoorOpened.java
+++ src/main/java/nl/ictm2a4/javagame/cachievements/FirstDoorOpened.java
@@ -3,12 +3,18 @@ package nl.ictm2a4.javagame.cachievements;
 import nl.ictm2a4.javagame.achievement.Achievement;
 import nl.ictm2a4.javagame.cevents.DoorOpenedEvent;
 import nl.ictm2a4.javagame.event.EventHandler;
+import nl.ictm2a4.javagame.screens.GameScreen;
+import nl.ictm2a4.javagame.services.achievements.AchievementsService;
 
 public class FirstDoorOpened extends Achievement {
 
     @EventHandler
     public void onDoorOpened(DoorOpenedEvent event) {
         achieve("First door opened");
-    }
 
+        int currentUser = GameScreen.getInstance().getCurrentUserId();
+        if(currentUser != 0) {
+            new AchievementsService().addAchievementToUser(1, currentUser);
+        }
+    }
 }
--- src/main/java/nl/ictm2a4/javagame/cachievements/LastLevelAchieved.java
+++ src/main/java/nl/ictm2a4/javagame/cachievements/LastLevelAchieved.java
@@ -4,6 +4,8 @@ import nl.ictm2a4.javagame.achievement.Achievement;
 import nl.ictm2a4.javagame.cevents.ReachedEndEvent;
 import nl.ictm2a4.javagame.event.EventHandler;
 import nl.ictm2a4.javagame.loaders.LevelLoader;
+import nl.ictm2a4.javagame.screens.GameScreen;
+import nl.ictm2a4.javagame.services.achievements.AchievementsService;
 
 public class LastLevelAchieved extends Achievement {
 
@@ -11,8 +13,11 @@ public class LastLevelAchieved extends Achievement {
     public void onLevelAchieved(ReachedEndEvent event) {
         if (event.getLevelID() == LevelLoader.DEFAULTLEVELAMOUNT - 1) {
             achieve("Last level survived");
+
+            int currentUser = GameScreen.getInstance().getCurrentUserId();
+            if(currentUser != 0) {
+                new AchievementsService().addAchievementToUser(3, currentUser);
+            }
         }
     }
-
-
 }
