2f0e4cf5a89ab122b7c79b3a44b279c882bc56ef
PaulPeriod <paulrill2000@outlook.com>
2020-05-26 16:01:35 +0200

    Fixed it!

--- src/main/java/nl/ictm2a4/javagame/gameobjects/TeleportationStone.java
+++ src/main/java/nl/ictm2a4/javagame/gameobjects/TeleportationStone.java
@@ -1,25 +1,20 @@
 package nl.ictm2a4.javagame.gameobjects;
 
-import nl.ictm2a4.javagame.cevents.ReachedEndEvent;
+import nl.ictm2a4.javagame.cevents.TeleportingEvent;
 import nl.ictm2a4.javagame.event.EventManager;
 import nl.ictm2a4.javagame.loaders.FileLoader;
 import nl.ictm2a4.javagame.loaders.JSONLoader;
 import nl.ictm2a4.javagame.loaders.LevelLoader;
-import nl.ictm2a4.javagame.raspberrypi.RaspberryPIController;
-import nl.ictm2a4.javagame.screens.EndScreen;
-import nl.ictm2a4.javagame.screens.GameScreen;
 import nl.ictm2a4.javagame.screens.Level;
 import nl.ictm2a4.javagame.screens.LevelEditor;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class TeleportationStone extends GameObject {
-    private long press;
+    private long prevTeleport;
+    private boolean teleported;
 
     @JSONLoader(JSONString = "teleportationStones")
     public TeleportationStone(Integer gridX, Integer gridY) {
@@ -32,34 +27,45 @@ public class TeleportationStone extends GameObject {
     }
 
     /**
-     * Override the GameObject checkCollideSingle, test if player collided with the endpoint
+     * Override the GameObject checkCollideSingle
      * @param gameObject GameObject which is colliding
      * @return result of isColliding check
      */
     @Override
     public boolean checkCollideSingle(GameObject gameObject, int x, int y) {
         boolean result = super.checkCollideSingle(gameObject,x,y);
-        return (result && gameObject instanceof Player);
+
+        if (result && gameObject instanceof Player && !teleported) {
+            Level level = LevelLoader.getInstance().getCurrentLevel().get();
+
+            Optional<TeleportationStone> stream = level.getGameObjects().stream()
+                .filter(gObject -> gObject instanceof TeleportationStone && (gObject.getX() != this.getX() || gObject.getY() != this.getY())).map(c -> (TeleportationStone)c).findFirst();
+
+            if (stream.isPresent() && level.getPlayer().isPresent()) {
+                TeleportationStone stone = stream.get();
+
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        level.getPlayer().get().setX(stone.getX());
+                        level.getPlayer().get().setY(stone.getY());
+                    }
+                }, 5);
+
+                stone.setTeleported(true);
+                this.setTeleported(true);
+
+                EventManager.getInstance().callEvent(new TeleportingEvent(this));
+            }
+        }
+
+        return false;
     }
 
     public void tick() {
-        GameObject[] collisions = checkCollideAllGameObjects(getX(), getY());
-        List<Integer> pressedKeys = GameScreen.getInstance().getPressedKeys();
-        String rpiButton = RaspberryPIController.getInstance().getPressedButton();
+        if (prevTeleport + 1000 <= System.currentTimeMillis() && teleported)
+            teleported = false;
 
-        if(press + 5000 <= System.currentTimeMillis() &&
-                Arrays.stream(collisions).anyMatch(gameObject -> gameObject instanceof Player) && (pressedKeys.contains(KeyEvent.VK_SPACE)  || rpiButton.equals("middle"))){
-            Level level = LevelEditor.getInstance().getLevel();
-
-            Optional<GameObject> stream = level.getGameObjects().stream()
-                    .filter(gObject -> gObject instanceof TeleportationStone && (gObject.getX() != this.getX() || gObject.getY() != this.getY())).findFirst();
-                if (stream.isPresent() && level.getPlayer().isPresent()) {
-                    GameObject stone = stream.get();
-                    level.getPlayer().get().setX(stone.getX());
-                    level.getPlayer().get().setY(stone.getY());
-                    press = System.currentTimeMillis();
-                }
-        }
     }
 
     @Override
@@ -83,5 +89,14 @@ public class TeleportationStone extends GameObject {
                 level.addGameObject(new TeleportationStone(gridX, gridY));
             }
         }.setRequireGround(true);
+    }
+
+    public void setTeleported(boolean teleported) {
+        this.teleported = teleported;
+        prevTeleport = System.currentTimeMillis();
+    }
+
+    public boolean getTeleported() {
+        return this.teleported;
     }
 }
